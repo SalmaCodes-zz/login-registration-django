283665ba96d418ca6fb340c6dd6e2e0ced73c8f6
Salma Suliman <ssuliman91@gmail.com>
2018-03-26 11:57:06 -0700

    Revert "More features."
    This reverts commit 7991f5b36f418694876d6a418a93173ac8c98934.

--- apps/users/views.py
+++ apps/users/views.py
@@ -7,25 +7,20 @@ import bcrypt
 # Create your views here.
 
 
-# '/' OR '/main': Shows login/registration page.
+# '/'
 def index(request):
     return render(request, 'users/index.html')
 
 
-# '/dashboard': Shows dashboard page.
-def dashboard(request):
-    if ('user_id' not in request.session.keys()):
-        return redirect('/main')
-
-    user = User.objects.get(id=request.session['user_id'])
+# '/success'
+def success(request):
     context = {
-        'user': user,
-        'other_wishlist': Item.objects.all().exclude(wished_by=user)
+        'user': User.objects.get(id=request.session['user_id'])
     }
-    return render(request, 'users/dashboard.html', context)
+    return render(request, 'users/success.html', context)
 
 
-# '/process': 'POST' method that proccesses the login/registration forms.
+# '/process', 'POST' method that proccesses the login/registration forms.
 def process(request):
     if request.method == 'POST':
         ftype = request.POST['type']
@@ -40,12 +35,13 @@ def process(request):
                 # Create User
                 password_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
                 user = User.objects.create(
-                    name=request.POST['name'],
-                    username=request.POST['username'],
-                    hired_date=request.POST['hired_date'],
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    email=request.POST['email'],
                     password=password_hash)
                 request.session['user_id'] = user.id
-                return redirect('/dashboard')
+                request.session['success'] = "registered"
+                return redirect('/success')
 
         # Login form
         elif (ftype == 'login'):
@@ -55,64 +51,7 @@ def process(request):
                     messages.error(request, error, extra_tags=tag)
                 return redirect('/')
             else: 
-                user = User.objects.filter(username=request.POST['username'])
+                user = User.objects.filter(email=request.POST['email'])
                 request.session['user_id'] = user[0].id
-                return redirect('/dashboard')
-
-
-# '/add_item': 'POST' method that creates a new item.
-def add_item(request):
-    if request.method == 'POST':
-        errors = Item.objects.basic_validator(request.POST)
-        if len(errors):
-            for tag, error in errors.iteritems():
-                messages.error(request, error, extra_tags=tag)
-            return redirect('/wish_items/create')
-        else:
-            # Create Item
-            user = User.objects.get(id=request.session['user_id'])
-            item = Item.objects.create(name=request.POST['name'], added_by=user)
-            item.wished_by.add(user)
-            return redirect('/dashboard')
-
-
-# '/wish_items/<id>'
-def show_item(request, id):
-    context = {
-        'item': Item.objects.get(id=id)
-    }
-    return render(request, 'users/show_item.html', context)
-
-
-# '/wish_items/<id>/delete': Deletes the item.
-def delete_item(request, id):
-    item = Item.objects.get(id=id)
-    item.delete()
-    return redirect('/dashboard')
-
-
-# '/wish_items/create', displays the create form page.
-def create_item(request):
-    return render(request, 'users/create_item.html')
-
-
-# '/wishlist_add/<id>': Adds the item with <id> to current user's wishlist.
-def wishlist_add(request, id):
-    item = Item.objects.get(id=id)
-    user = User.objects.get(id=request.session['user_id'])
-    item.wished_by.add(user)
-    return redirect('/dashboard')
-
-
-# '/wishlist_remove/<id>': Removes the item with <id> from current user's wishlist.
-def wishlist_remove(request, id):
-    item = Item.objects.get(id=id)
-    user = User.objects.get(id=request.session['user_id'])
-    item.wished_by.remove(user)
-    return redirect('/dashboard')
-
-    
-# '/logout'
-def logout(request):
-    request.session.pop('user_id')
-    return redirect('/main')
+                request.session['success'] = "loggin in"
+                return redirect('/success')
